5e1925c6b9e4b60d88bebae7f50ce3dcfd6dfcef
Terence <terence.chia@harro.com>
2020-04-15 00:51:32 +1000

    Create cx.sh

---
+++ security/cx.sh
@@ -0,0 +1,9 @@
+
+# Security Validation
+
+- this is separate from user creation.
+- validates base infrastructure
+- validates user access
+- validates anomalies
+- should it access dynamo db? Needs secure access.
+
