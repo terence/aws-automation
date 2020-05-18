8ee6d580bc59a1aae58c702ca03dcd5271ed84ff
Terence <terence@192-168-1-5.tpgi.com.au>
2020-05-18 12:40:58 +1000

    Scoped refined

--- lambda/cx.sh
+++ lambda/cx.sh
@@ -1,4 +1,4 @@
-# AWS Scripts Command-line Assistant
+# AWS Lambda Scripts Command-line Assistant
 #================================================================
 clear
 #source ./vars.sh
@@ -8,24 +8,40 @@ PWD=pwd
 PROFILE="ipadev"
 REGION="ap-southeast-2"
 OUTPUT="json"
-EMR_CLUSTER_ID="xxx"
-
-
+FUNCTION_CODE="lambda-vpc"
+FUNCTION_NAME="terence-vpc1"
 
 echo =============================================================
 echo Hi $USER@$HOSTNAME. You are in $PWD directory.
 echo -------------------------------------------------------------
 echo 001 : AWS Configure
 echo 002 : AWS S3 List
-echo 003 : AWS STS Assume Role
+echo 003 : AWS Lambda
 echo ----------------------------------------------
 echo 100 : AWS Lambda Account Settings
 echo 101 : AWS Lambda List Functions
 echo 102 : AWS Lambda List Layers
 echo ----------------------------------------------
-echo 150 : AWS STEPFUNCTIONS describe-actiivity
-echo 151 : AWS STEPFUNCTIONS xxx
-echo 152 : AWS STEPFUNCTIONS xxx
+echo 110 : AWS Lambda lambda-vpc Create Function 
+echo 111 : AWS Lambda lambda-vpc Update Function Code
+echo 112 : AWS Lambda lambda-vpc Invoke Function
+echo 113 : AWS Lambda lambda-vpc Delete Function
+echo 114 : AWS Lambda lambda-vpc Get Function Configuration
+echo 115 : AWS Lambda lambda-vpc Update Function Configuration
+echo ----------------------------------------------
+echo 120 : AWS Lambda lambda-iam Create Function 
+echo 121 : AWS Lambda lambda-iam Update Function Code
+echo 122 : AWS Lambda lambda-iam Invoke Function
+echo 123 : AWS Lambda lambda-iam Delete Function
+echo 124 : AWS Lambda lambda-iam Get Function Configuration
+echo 125 : AWS Lambda lambda-iam Update Function Configuration
+echo ----------------------------------------------
+echo 200 : AWS STEPFUNCTIONS describe-actiivity
+echo 201 : AWS STEPFUNCTIONS xxx
+echo 202 : AWS STEPFUNCTIONS xxx
+echo ----------------------------------------------
+echo 900 : V-Env Create
+echo 901 : V-Env Activate
 echo ----------------------------------------------
 echo Enter [Selection] to continue
 echo =============================================================
@@ -40,6 +56,7 @@ fi
 if [ -n "$2" ]; then
   PROFILE=$2
 fi
+
 
 echo Your selection is : $SELECTION.
 echo Your profile is : $PROFILE.
@@ -59,18 +76,10 @@ case "$SELECTION" in
   aws s3 ls --profile $PROFILE | wc -l
 
   #aws s3 ls s3://bucketname
-  #aws s3 cp 
+  #aws s3 cp
   # aws s3 sync local s3://remote
   ;;
 
-
-"003" )
-  echo "===== AWS Assume Role:" $PROFILE
-  # aws sts assume-role --role-arn "arn:aws:iam::xxxxxxxxxxxx:role/AWSAdmin" --role-session-name AWSCLI-Session
-  # aws sts get-caller-identity --profile ipadev 
-  ;;
-
-    
 "100" )
   echo "===== AWS Lambda Get Account Settings:" $PROFILE
   aws lambda get-account-settings \
@@ -94,12 +103,87 @@ case "$SELECTION" in
     --output $OUTPUT
   ;;
 
-"150" )
-  echo "===== AWS APIGATEWAY get-rest-apis:" $PROFILE
-  aws apigateway get-rest-apis \
+
+"110" )
+  echo "===== AWS Lambda Create Function:" $PROFILE
+  FUNCTION_CODE="lambda-vpc"
+  FUNCTION_NAME="terence-vpc1"
+  cd $FUNCTION_CODE
+  rm $FUNCTION_CODE.zip
+  zip -r $FUNCTION_CODE.zip .
+  cd ..
+  aws lambda create-function \
+    --function-name $FUNCTION_NAME \
+    --runtime python3.8 \
+    --zip-file fileb://$FUNCTION_CODE/$FUNCTION_CODE.zip \
+    --handler lambda_function.lambda_handler \
+    --role arn:aws:iam::832435373672:role/service-role/lambda-helloworld1-role-rpwjd1ud \
     --profile $PROFILE \
-    --output json
+    --output $OUTPUT
   ;;
+
+
+"111" )
+  echo "===== AWS Lambda Update Function Code:" $PROFILE
+  FUNCTION_CODE="lambda-vpc"
+  FUNCTION_NAME="terence-vpc1"
+  cd $FUNCTION_CODE
+  rm $FUNCTION_CODE.zip
+  zip -r $FUNCTION_CODE.zip .
+  cd ..
+  aws lambda update-function-code \
+    --function-name $FUNCTION_NAME \
+    --zip-file fileb://$FUNCTION_CODE/$FUNCTION_CODE.zip \
+    --profile $PROFILE \
+    --output $OUTPUT
+  ;;
+
+"112" )
+  echo "===== AWS Lambda Invoke Function:" $PROFILE
+  FUNCTION_CODE="lambda-vpc"
+  FUNCTION_NAME="terence-vpc1"
+	aws lambda invoke \
+    --function-name $FUNCTION_NAME \
+		--profile $PROFILE \
+    --output $OUTPUT \
+		response.json
+  ;;
+
+#    --invocation-type Event \
+#		-- payload '{"key1":"value1"}' \
+
+"113" )
+  echo "===== AWS Lambda Delete Function:" $PROFILE
+  FUNCTION_NAME="terence-vpc1"
+  aws lambda delete-function \
+    --function-name $FUNCTION_NAME \
+    --profile $PROFILE \
+    --output $OUTPUT
+  ;;
+
+
+"114" )
+  echo "===== AWS Lambda Get Function Configuration:" $PROFILE
+  FUNCTION_NAME="terence-vpc1"
+  aws lambda get-function-configuration \
+    --function-name $FUNCTION_NAME \
+    --profile $PROFILE \
+    --output $OUTPUT
+  ;;
+
+
+"115" )
+  echo "===== AWS Lambda Update Function Configuration:" $PROFILE
+  FUNCTION_NAME="terence-vpc1"
+  aws lambda update-function-configuration \
+    --function-name $FUNCTION_NAME \
+    --memory-size 256
+		--profile $PROFILE \
+    --output $OUTPUT
+  ;;
+
+
+
 
 
 
@@ -126,6 +210,7 @@ case "$SELECTION" in
   echo "Not a recognized option."
   ;;
 esac
+
 
 
 
